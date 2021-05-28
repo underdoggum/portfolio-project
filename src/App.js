95a033e5a7aa051a1b98a7eaf61723290df5f60b
Nathan <natenoack@gmail.com>
2021-05-27 21:17:48 -0400

    logo and bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
+
+//from the portfolio project via
+//https://www.youtube.com/watch?v=vD72tRK8mC4
 
 function App() {
   return (
-    <h2>Portfolio Project</h2>
+    <Navbar />
   );
 }
 
