dcbdd40780354eaad4bd9dbfe6cc90835e83f150
Nathan Noack <nate@pop-os.localdomain>
2021-11-16 23:22:29 -0500

    styling changes, changed text on main info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 import React from "react"
 import Typed from "react-typed"
+import { Link } from "react-scroll"
 
 const Header = () => {
   return (
     <div id="header" className="header-wrapper">
       <div className="main-info">
         <h1>
-          Front-End Web Development
+          Full-Stack Web Developer
         </h1>
         <Typed
           className="typed-text"
-          strings={["y=mx+b", "Mitosis", "Flashpoints", "AP Calculus"]}
-          typeSpeed={40}
+          strings={["React", "Node.js", "MongoDB", "ExpressJS", "jQuery", "SASS", "Bootstrap", "Git/Github", "RESTful APIs", "MVC Architecture", "Scrum/Agile Development", "Terminal/Bash", "Wireframing"]}
+          typeSpeed={50}
           backSpeed={60}
           loop
         />
-        <a href="#" className="btn-main-offer">Contact Me</a>
+        <Link to="contacts" offset={-100} className="nav-link" href="#" className="btn-main-offer">Contact Me</Link>
       </div>
     </div>
   )
